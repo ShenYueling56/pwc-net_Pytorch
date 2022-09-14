fe5b9c9fe66effc61563a33614e18003eaaca070
shenyueling56 <3247253259@qq.com>
2022-09-14 15:18:57 +0800

    setup

--- script_pwc.py
+++ script_pwc.py
@@ -36,8 +36,8 @@ def flow2rgb(flow_map_np, max_value=None):
     rgb = cv2.cvtColor(hsv, cv2.COLOR_HSV2RGB)
     return rgb
 
-im1_fn = 'data/frame_0010.png';
-im2_fn = 'data/frame_0011.png';
+im1_fn = 'data/input4_1.jpg';
+im2_fn = 'data/input4_2.jpg';
 flow_fn = 'data/frame_0010.flo';
 
 if len(sys.argv) > 1:
@@ -93,4 +93,4 @@ flo = flo.cpu().data.numpy()
 # writeFlowFile(flow_fn, flo)
 rgb_flow = flow2rgb(flo)
 # Image.fromarray(rgb_flow, 'RGB').show()
-cv2.imwrite('data/rgb_flow.png', rgb_flow)
+cv2.imwrite('data/rgb_flow4_1.png', rgb_flow)
